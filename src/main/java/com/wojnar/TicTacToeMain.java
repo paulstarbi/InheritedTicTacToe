ab87d545c5209b4eab900208dd4e6bfba1771ed4
Bartlomiej_Wojnar@epam.com <me@example.com>
2017-06-27 14:18:37 +0200

    initial commit with pom.xml, readme file, hellowrold app

---
+++ src/main/java/com/wojnar/TicTacToeMain.java
@@ -0,0 +1,10 @@
+package com.wojnar;
+
+/**
+ * Created by bartomiej on 27.06.17.
+ */
+public class TicTacToeMain {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
